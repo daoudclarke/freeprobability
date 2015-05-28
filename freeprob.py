c1057a85048bd9f2e1cc6b0b8124560447391573
Daoud Clarke <daoud.clarke@gmail.com>
2015-05-28 17:13:39 +0100

    Initial commit

---
+++ freeprob.py
@@ -0,0 +1,58 @@
+
+ # phi( (a - phi(a))(b - phi(b)) ) = 0
+ # phi( (ab - phi(a)*b - a*phi(b) + phi(a)*phi(b)) ) = 0
+ # phi(ab) - phi(a)*phi(b) - phi(a)*phi(b) + phi(a)*phi(b) = 0
+
+from itertools import product, groupby
+
+
+
+
+def compute_free_prob(probs, input_sequence):
+    """
+    Compute the probability of the sequence, expressed as a
+    space-separated string. Elements starting with m_ and s_ are
+    treated as free respectively.
+
+    Input: probs: dictionary from tuples of symbols to probabilities
+           sequence: a tuple for which to compute the probability
+    """
+    multiplier = 1.0
+    for s in input_sequence:
+        if type(s) == float:
+            multiplier *= s
+    sequence = [s for s in input_sequence if type(s) != float]
+
+    if len(sequence) == 0:
+        return multiplier
+
+    free_sequence = get_free_sequences(sequence)
+
+    if len(free_sequence) == 1:
+        return multiplier*probs[free_sequence[0]]
+
+    poly = [(s, -probs[s]) for s in free_sequence]
+    expressions = product(*poly)
+
+    total = 0.0
+    # Skip the first expression, which is what we're trying to compute
+    next(expressions)
+    for expression in expressions:
+        unpacked = []
+        for e in expression:
+            if type(e) == float:
+                unpacked.append(e)
+            else:
+                unpacked += list(e)
+        total -= compute_free_prob(probs, unpacked)
+
+    print input_sequence, multiplier*total
+    return multiplier * total
+
+
+def get_free_sequences(sequence):
+    """
+    Return tuples of sequences which combine freely (which we are
+    assuming have different first characters).
+    """
+    return [tuple(group) for _, group in groupby(sequence, lambda s: s[0])]
---
+++ test_freeprob.py
@@ -0,0 +1,60 @@
+
+from freeprob import compute_free_prob, get_free_sequences
+from collections import defaultdict
+
+
+# def test_compute_free_prob():
+#     probs = {
+#         'm_john': 0.5,
+#         'm_sneezed': 0.5,
+#         'm_someone': 0.4,
+#         'm_coughed': 0.6,
+#         'm_john m_
+
+def test_compute_free_prob():
+    probs = {('m_a',): 0.5, ('s_a',): 0.5}
+    prob = compute_free_prob(probs, ('m_a', 's_a'))
+    assert prob == 0.25
+
+
+def test_free_sequences():
+    sequence = ['s_a', 's_b', 'm_a', 's_b']
+    free = get_free_sequences(sequence)
+    assert free == [('s_a', 's_b'), ('m_a',), ('s_b',)]
+
+
+def test_sneeze():
+    #sequence = 'm_john s_n m_sneezed s_v m_someone s_n m_coughed s_v'.split()
+    sequence = 'm_john s_n m_sneezed s_v'.split()
+    #probs = defaultdict(float, {
+    probs = {
+        ('m_john',): 0.5,
+        ('m_sneezed',): 0.4,
+        ('m_sneezed', 'm_someone'): 0.4,
+        ('m_john', 'm_sneezed'): 0.3,
+        ('m_someone',): 0.7,
+        ('m_coughed',): 0.6,
+        ('m_john', 'm_someone'): 0.5,
+        ('m_sneezed', 'm_coughed'): 0.4,
+        ('m_someone', 'm_coughed'): 0.4,
+        ('m_john', 'm_coughed'): 0.5,
+        ('m_john', 'm_sneezed', 'm_someone'): 0.3,
+        ('m_sneezed', 'm_someone', 'm_coughed'): 0.2,
+        ('m_john', 'm_sneezed', 'm_coughed'): 0.3,
+        ('m_john', 'm_someone', 'm_coughed'): 0.2,
+        ('m_john', 'm_sneezed', 'm_someone', 'm_coughed'): 0.2,
+        ('s_n',): 0.9,
+        ('s_v',): 0.9,
+        ('s_n', 's_v'): 0.8,
+        ('s_v', 's_n'): 0.5,
+        ('s_n', 's_n'): 0.1,
+        ('s_v', 's_v'): 0.1,
+        ('s_n', 's_v', 's_n'): 0.4,
+        ('s_n', 's_n', 's_v'): 0.1,
+        ('s_v', 's_n', 's_v'): 0.4,
+        ('s_n', 's_v', 's_v'): 0.1,
+        ('s_n', 's_v', 's_n', 's_v'): 0.3,
+        }
+
+    prob = compute_free_prob(probs, sequence)
+    print prob
